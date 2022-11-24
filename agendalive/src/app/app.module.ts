daaee0ae83163a08f4970e4ac6d3faa71925cdf0
lauromiottojr <junior.miotto36@gmail.com>
2022-11-24 13:59:48 -0300

    Creating HomeComponent

--- agendalive/src/app/app.module.ts
+++ agendalive/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
